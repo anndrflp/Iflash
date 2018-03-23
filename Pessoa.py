49b859d79283d5a7af4746bcc1739c12e4510bb8
Anderson Felipe <30373732+anndrflp@users.noreply.github.com>
2018-03-23 19:30:22 -0300

    Estudando

--- Pessoa.py
+++ Pessoa.py
@@ -1,5 +1,13 @@
-class Pessoa():
+class Pessoa(object):
 
-    idade = 0
+    def __init__(self):
+        self.nome = 'Anderson'
+        self.idade = 20
+        print('Const. chamado com sucesso')
 
+    def imprime(self):
+        print('Ola meu nome : %s e eu tenho %d' % (self.nome, self.idade))
+
+anderson = Pessoa()
+anderson.imprime()
 Pessoa()
